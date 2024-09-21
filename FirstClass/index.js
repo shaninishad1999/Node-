fbab679b9884cc06d6ce3c01f9445988250920ab
shaninishad1999 <shaninishad1999@gmail.com>
2024-09-21 16:00:05 +0530

    Node Start

---
+++ FirstClass/index.js
@@ -0,0 +1,5 @@
+const http = require("http");
+http.createServer((req, res) => {
+    res.write("<h1> This hello this  kamlesh  helllllllllllll jkdslfahlfjsklfhaslkdjfaslfkhsjl  hllepksdfkksdl    updatedwq is  node program")
+    res.end();
+}).listen(9000);
