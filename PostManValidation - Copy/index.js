4f7d004ad8628f74ccd18e6814fd146583348a76
shaninishad1999 <shaninishad1999@gmail.com>
2024-09-28 07:46:48 +0530

    copy delted

--- PostManValidation - Copy/index.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const app=express();
-
-const StuRoute=require("./routes/studentsRoute")
-
-app.use("/students",StuRoute)
-app.listen(8080,()=>{
-    console.log("server run on 8080")
-})
